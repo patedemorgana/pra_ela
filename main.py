21086a0bfd003c0e94b8d75762f70c4a6ba75444
patedemorgana <gabriel.volpate@gmail.com>
2022-11-28 22:14:53 -0300

    sdW

---
+++ main.py
@@ -0,0 +1,15 @@
+def namoradinha_futura():
+    começo = input("OI ")
+    a = input("Preciso te perguntar um bagulho")
+    print("Yo gosto bastante de um ser humano chamado você")
+    pergunta = input("Vc gosta de mim?!?!?!?: ")
+    Aceitar = False
+    while not Aceitar:
+        if pergunta == 'sim':
+            print("Por dentro : AAAAAAAAAAAAAAAAAAAAAAA")
+            print("Por fora : Ha, eu sabia, vamos namorar")
+            Accept = True
+        else:
+            pergunta = input("Vc gosta de mim?!?!?!?: ")
+
+namoradinha_futura()
